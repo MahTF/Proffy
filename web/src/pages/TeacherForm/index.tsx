984a5a54de0a009bbd4e564291a1e06245ee34a1
Marcelo Ferreira <mah.tf@hotmail.com>
2020-08-03 16:39:59 -0300

    Adicionado react-router-dom para roteamento

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+function TeacherForm() {
+    return (
+        <h1>Teacher Form works!</h1>
+    );
+}
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+function TeacherList() {
+    return (
+        <h1>Teacher List works!</h1>
+    );
+}
+
+export default TeacherList;

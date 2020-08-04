9475de022aeaf13e834616030b8ebda9785c897b
Marcelo Ferreira <mah.tf@hotmail.com>
2020-08-04 19:58:31 -0300

    Adicionado a rota para criação de aulas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-app.use(express.json);
-
-app.get('/users', (req, res) => {
-    const users = [
-        { name: 'Marcelo', birthday: 'Jan/12' }
-    ];
-
-    return res.json(users);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);

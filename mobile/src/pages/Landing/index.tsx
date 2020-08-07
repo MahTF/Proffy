4c132ba147514014109677af1d6968f3bf4ac032
Marcelo Ferreira <mah.tf@hotmail.com>
2020-08-07 20:24:41 -0300

    Adicionando axios no mobile para pegar conexoes

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -6,10 +6,19 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 import styles from './styles';
 
 function Landing() {
     const navigation = useNavigation();
+    const [totalConnection, setTotalConnection] = useState(0);
+
+    useEffect(() => {
+        api.get('/connections').then(res => {
+            const { total } = res.data;
+            setTotalConnection(total);
+        });
+    }, [])
 
     function handleNavigationToGiveClassesPage() {
         navigation.navigate('GiveClasses');
@@ -41,7 +50,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 200 conexões já realizadas {' '}
+                Total de {totalConnection} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
